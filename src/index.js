d6267318a26f8fac9ddf463ae4d78bb220ca169c
Adam <grenador@gmail.com>
2019-07-08 23:26:35 -0400

    Fixed URL paths

--- src/index.js
+++ src/index.js
@@ -3,12 +3,12 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
-import { BrowserRouter as Router } from 'react-router-dom'
+import { BrowserRouter as Router } from 'react-router-dom';
 
 ReactDOM.render(
-<Router>
+  <Router basename={'/tech-challenge'}>
     <App />
-</Router>, document.getElementById('root'));
+  </Router>, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
