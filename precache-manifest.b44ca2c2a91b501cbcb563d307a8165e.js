f4c5922c031aa54d67a1af211173cddcb36d7020
Adam <grenador@gmail.com>
2019-07-08 22:59:05 -0400

    Updates

---
+++ precache-manifest.b44ca2c2a91b501cbcb563d307a8165e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1734d6e22676d86b3f9213114d7d60cf",
+    "url": "/tech-challenge/index.html"
+  },
+  {
+    "revision": "81249a643435d1390094",
+    "url": "/tech-challenge/static/css/main.9816c556.chunk.css"
+  },
+  {
+    "revision": "b08a2248642f7f8b2310",
+    "url": "/tech-challenge/static/js/2.0153976c.chunk.js"
+  },
+  {
+    "revision": "81249a643435d1390094",
+    "url": "/tech-challenge/static/js/main.1316f168.chunk.js"
+  },
+  {
+    "revision": "65f89f93ba52fc3c9e3b",
+    "url": "/tech-challenge/static/js/runtime~main.657c240c.js"
+  },
+  {
+    "revision": "3e29177e4e905a5d868311c633d42821",
+    "url": "/tech-challenge/static/media/nophotoavailable-icon.3e29177e.jpg"
+  }
+]);
